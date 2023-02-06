98cb97cc23ba81058d87799ddf7459dee1895177
Tristan Thorburn <tristanthorburn@gmail.com>
2023-02-06 18:07:47 -0500

    toggle for draggable

--- src/components/backdash/TableMapData.js
+++ src/components/backdash/TableMapData.js
@@ -1,11 +1,33 @@
 import { Link } from 'react-router-dom';
+import TableMap from '../summary/table/TableMap';
+import { useState } from 'react';
 
 const TableMapData = () => {
+    const [ mapUpdateable, setMapUpdateable ] = useState(false)
+
+    const enableUpdate = () => {
+        setMapUpdateable(true)
+    }
+
+    const disableUpdate = () => {
+        setMapUpdateable(false)
+    }
+
     return(
-        <div>
-            <h2>Tables and Map Setup</h2>
-            <p>Under Construction</p>
+        <div className='tableBackDash'>            
             <Link to='/backend-dash'>Return to Backend Dashboard</Link>
+
+            <h2>Tables and Map Setup</h2>
+
+            {mapUpdateable
+                ? <button onClick={disableUpdate}>Disable Updates</button>
+                : <button onClick={enableUpdate}>Update Map</button>
+            }
+            
+            
+            <div className='tableTab'>
+                <TableMap updateable={mapUpdateable}/>
+            </div>            
         </div>
     )
 }
--- src/components/summary/table/TableMap.js
+++ src/components/summary/table/TableMap.js
@@ -1,16 +1,38 @@
-const TableMap = () => {
+import { useEffect } from 'react';
+
+const TableMap = (props) => {
+
+    useEffect(() => {
+        const tables = document.querySelectorAll('.table')
+
+        if(props.updateable === true){
+            tables.forEach((table) => {
+                table.setAttribute('draggable', true)
+            })
+            // tables.addEventListener.forEach((table) => {
+
+            // })
+        }
+        if(props.updateable === false){
+            tables.forEach((table) => {
+                table.setAttribute('draggable', false)
+            })
+        }
+        
+    },[props.updateable])
+
     return(
         <ul className='tableMap'>
-            <li className='tableOne' draggable='true'><p>Table 1</p></li>
-            <li className='tableTwo'><p>Table 2</p></li>
-            <li className='tableThree'><p>Table 3</p></li>
-            <li className='tableFour'><p>Table 4</p></li>
-            <li className='tableFive'><p>Table 5</p></li>
-            <li className='tableSix'><p>Table 6</p></li>
-            <li className='tableSeven'><p>Table 7</p></li>
-            <li className='tableEight'><p>Table 8</p></li>
-            <li className='tableNine'><p>Table 9</p></li>
-            <li className='tableTen'><p>Table 10</p></li>
+            <li className='table tableOne'><p>Table 1</p></li>
+            <li className='table tableTwo'><p>Table 2</p></li>
+            <li className='table tableThree'><p>Table 3</p></li>
+            <li className='table tableFour'><p>Table 4</p></li>
+            <li className='table tableFive'><p>Table 5</p></li>
+            <li className='table tableSix'><p>Table 6</p></li>
+            <li className='table tableSeven'><p>Table 7</p></li>
+            <li className='table tableEight'><p>Table 8</p></li>
+            <li className='table tableNine'><p>Table 9</p></li>
+            <li className='table tableTen'><p>Table 10</p></li>
         </ul>
     )
 }
