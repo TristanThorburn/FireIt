e5a0cad80905c0fc31452f28603b8a270bc39333
Tristan Thorburn <tristanthorburn@gmail.com>
2023-02-16 18:44:52 -0500

    populate check with some data, yay

--- src/components/frontdash/menu_tab_components/TableCheck.js
+++ src/components/frontdash/menu_tab_components/TableCheck.js
@@ -1,7 +1,7 @@
 import { useTable } from '../../../contexts/TableContext';
 import { useAuth } from '../../../contexts/AuthContext';
 import { db } from '../../../firebase';
-import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { doc, getDoc, orderBy, onSnapshot, query, collection } from 'firebase/firestore';
 import { useState, useEffect } from 'react';
 
 const TableCheck = () => {
@@ -9,17 +9,32 @@ const TableCheck = () => {
     const { contextTable } = useTable();
     const [ tableData, setTableData ] = useState([])
     const [ serverData, setServerData ] = useState({})
+    const [ checkData, setCheckData ] = useState([])
     // query where()
-    const checkRef = doc(db, 'checks', `${serverData.employeeNumber}`, `${tableData.name}`, 'seat')
+    // const checkRef = doc(db, 'checks', `${serverData.employeeNumber}`, `${tableData.name}`, 'seat')
+    const checkCollectionRef = 
+        collection(db, 'checks', `${serverData.employeeNumber}`, `${tableData.name}`)
+
+    useEffect(() => {
+        const q = query(checkCollectionRef, orderBy('seatNumber'));
+        const unsubscribe = onSnapshot(q, snapshot => {
+            setCheckData(snapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data()
+            })))
+        })
+        return unsubscribe
+    },[checkCollectionRef])
 
     const handleTest = async () => {
-        // console.log('server:', serverData, 'table:', tableData);
-        setDoc(checkRef, {
-            seat:true,
-            seatNumber:'1',
-            items:['chicken strips', 'burger', 'spaghetti'],
-            checkTotal:'15',
-        })
+        // console.log('server:', serverData.employeeNumber, 'table:', tableData.name);
+        // setDoc(checkRef, {
+        //     seat:true,
+        //     seatNumber:'1',
+        //     items:['chicken strips', 'burger', 'spaghetti'],
+        //     checkTotal:'15',
+        // })
+        console.log(checkData[0].data.items)
     }
 
     // Check which table is in tableContext
@@ -58,6 +73,17 @@ const TableCheck = () => {
                     <h2>No Table Selected</h2>
                 </div>
             }
+            <ul>
+                {checkData?.map(seat =>
+                    <div key={seat.id}>
+                        <h4>Seat: {seat.data.seatNumber}</h4>
+                        <p>Total: {seat.data.checkTotal}</p>
+                        <div>{seat.data.items.map((item, i) => 
+                            <p key={i}>{item}</p>)
+                        }</div>
+                    </div> 
+                )}
+            </ul>
         </div>
     )
 }
